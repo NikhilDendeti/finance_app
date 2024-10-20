e97c02ef651059ed6157ce534b5cc8a55d6115ce
nikhil <dendetinikhil@hmail.com>
2024-10-20 22:52:24 +0530

    MODELS_1

--- finanace_backend/admin.py
+++ finanace_backend/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import *
-
+from finanace_backend.models import *
 
 # Register your models here.
 admin.site.register(User)
@@ -9,7 +8,5 @@ admin.site.register(UserProfile)
 
 admin.site.register(Location)
 
-admin.site.register(LocationBasedExpenditure)
 
-admin.site.register(Transaction)
-admin.site.register(Expense)
+admin.site.register(LocationBasedExpenditure)
