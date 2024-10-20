85cb883567b2aa784dd5b74e0869e3ed0030d811
nitesh <sabbavarapunitesh636@gmail.com>
2024-10-20 21:57:07 +0530

    added Expense AND Transactions models.

--- finanace_backend/models.py
+++ finanace_backend/models.py
@@ -56,6 +56,7 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
+    month = models.DateField(auto_now=True)
 
     location = models.CharField(max_length=150, choices=LOCATION_CHOICES,
                                 default='hyderabad')
@@ -158,3 +159,29 @@ class LocationBasedExpenditure(models.Model):
 
     def __str__(self):
         return f"{self.location.name} - {self.get_category_display()} - {self.gender}"
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(max_length=50, choices=Category.choices())
+    description = models.TextField(blank=True, null=True)
+    # amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.category}: {self.amount}"
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=10, choices=(
+                    ('income', 'Income'), ('expense', 'Expense')))
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.transaction_type} - {self.amount}"
+
+    class Meta:
+        ordering = ['-date']
