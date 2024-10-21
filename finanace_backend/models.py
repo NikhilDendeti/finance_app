bbd3f31ac315a2c86c68b795d9a057566cc1c20e
nikhil <dendetinikhil@hmail.com>
2024-10-21 10:15:53 +0530

    MODELS_1

--- finanace_backend/models.py
+++ finanace_backend/models.py
@@ -178,12 +178,10 @@ class Expense(models.Model):
 class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     transaction_type = models.CharField(max_length=10, choices=(
-                    ('income', 'Income'), ('expense', 'Expense')))
+        ('income', 'Income'), ('expense', 'Expense')))
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now=True)
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.transaction_type} - {self.amount}"
-
-
